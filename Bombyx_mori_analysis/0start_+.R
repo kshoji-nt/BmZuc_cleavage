839402c4156cdbd3965b05637e8a84baec5d83d3
kshoji-nt <56252412+kshoji-nt@users.noreply.github.com>
2019-10-10 16:59:05 +0900

    Update 0start_+.R

--- Bombyx_mori_analysis/0start_+.R
+++ Bombyx_mori_analysis/0start_+.R
@@ -16,7 +16,7 @@ library(Biostrings)
 bed <- read.table(in_f)
 bed2<- bed[bed[,2] == 0 & bed[,6] == "+"& bed[,3]>19,c(1,3)] #Extract rows starting at 0, with direction +, and length of 20 or more
 
-#Create a table of length distribution for each array
+#Create a table of length distribution for each sequence
 BOX <- matrix(0,length(reads1),26)
 NAME <-names(reads1)
 rownames(BOX)  <-NAME
@@ -28,7 +28,7 @@ for (i in 1:length(reads1)){
 	tmp <- lisc[[names(reads1[i])]]
 	BOX[names(reads1[i]),names(tmp)] <- tmp
 	if(is.element(i,sec)){
-		cat("=")
+		cat("=") #Display = for every 1/100 progress
 	}
 }
 
